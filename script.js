f2f610d9bce9079b727dfb4759aa77c4b3e8c6a6
dadiallo <d.abdoulayead28@gmail.com>
2022-12-08 23:10:07 +0000

    tous les sections ajouté

--- script.js
+++ script.js
@@ -96,3 +96,232 @@ Array.from(aboutMeTextContent).forEach((char) => {
   });
 });
 // Fin a propos de moi Text
+
+// Projects
+const container = document.querySelector(".container");
+const projects = document.querySelectorAll(".project");
+const projectHideBtn = document.querySelector(".project-hide-btn");
+
+//Pour chaque projet que ca scrole quand on survole
+projects.forEach((project, i) => {
+  project.addEventListener("mouseenter", () => {
+    // L'image du projet - la taille du projet + 20
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
+  });
+
+  project.addEventListener("mouseleave", () => {
+    project.firstElementChild.style.top = "2rem";
+  });
+
+  // Grandir Project Image
+  project.addEventListener("click", () => {
+    const bigImgWrapper = document.createElement("div");
+    bigImgWrapper.className = "project-img-wrapper";
+    container.appendChild(bigImgWrapper);
+    //Variable pour acceder au different image
+    const bigImg = document.createElement("img");
+    bigImg.className = "project-img";
+    const imgPath = project.firstElementChild.getAttribute("src").split(".")[0];
+    bigImg.setAttribute("src", `${imgPath}-big.jpg`);
+    bigImgWrapper.appendChild(bigImg);
+    document.body.style.overflowY = "hidden";
+
+    document.removeEventListener("scroll", scrollFn);
+
+    mouseCircle.style.opacity = 0;
+
+    progressBarFn(bigImgWrapper);
+
+    bigImgWrapper.onscroll = () => {
+      progressBarFn(bigImgWrapper);
+    };
+
+    projectHideBtn.classList.add("change");
+
+    projectHideBtn.onclick = () => {
+      projectHideBtn.classList.remove("change");
+      bigImgWrapper.remove();
+      document.body.style.overflowY = "scroll";
+
+      document.addEventListener("scroll", scrollFn);
+
+      progressBarFn();
+    };
+  });
+  // End of Big Project Image
+
+  i >= 6 && (project.style.cssText = "display: none; opacity: 0");
+});
+
+// Projects Button
+const section3 = document.querySelector(".section-3");
+const projectsBtn = document.querySelector(".projects-btn");
+const projectsBtnText = document.querySelector(".projects-btn span");
+let showHideBool = true;
+
+const showProjects = (project, i) => {
+  setTimeout(() => {
+    project.style.display = "flex";
+    section3.scrollIntoView({ block: "end" });
+  }, 600);
+
+  setTimeout(() => {
+    project.style.opacity = "1";
+  }, i * 200);
+};
+
+const hideProjects = (project, i) => {
+  setTimeout(() => {
+    project.style.display = "none";
+    section3.scrollIntoView({ block: "end" });
+  }, 1200);
+
+  setTimeout(() => {
+    project.style.opacity = "0";
+  }, i * 100);
+};
+
+projectsBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  projectsBtn.firstElementChild.nextElementSibling.classList.toggle("change");
+
+  showHideBool
+    ? (projectsBtnText.textContent = "Voir+")
+    : (projectsBtnText.textContent = "Voir-");
+
+  projects.forEach((project, i) => {
+    // Si le nombre de projet  6>= afficher ca
+    i >= 6 &&
+      (showHideBool ? showProjects(project, i) : hideProjects(project, i));
+  });
+  showHideBool = !showHideBool;
+});
+// Fin bouton Projet
+// Fin  Projects
+// Section 4
+document.querySelectorAll(".service-btn").forEach((service) => {
+  service.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const serviceText = service.nextElementSibling;
+    serviceText.classList.toggle("change");
+
+    const rightPosition = serviceText.classList.contains("change")
+      ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})`
+      : 0;
+
+    service.firstElementChild.style.right = rightPosition;
+  });
+});
+// Fin Section 4
+// Section 5
+// Form
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Let's Talk";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
+// End of Form
+// Slideshow
+const slideshow = document.querySelector(".slideshow");
+
+setInterval(() => {
+  const firstIcon = slideshow.firstElementChild;
+
+  firstIcon.classList.add("faded-out");
+
+  const thirdIcon = slideshow.children[3];
+
+  thirdIcon.classList.add("light");
+
+  thirdIcon.previousElementSibling.classList.remove("light");
+
+  setTimeout(() => {
+    slideshow.removeChild(firstIcon);
+
+    slideshow.appendChild(firstIcon);
+
+    setTimeout(() => {
+      firstIcon.classList.remove("faded-out");
+    }, 500);
+  }, 500);
+}, 3000);
+// End of Slideshow
+
+// Form Validation
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
+
+const error = (input, message) => {
+  input.nextElementSibling.classList.add("error");
+  input.nextElementSibling.textContent = message;
+};
+
+const success = (input) => {
+  input.nextElementSibling.classList.remove("error");
+};
+
+const checkRequiredFields = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      error(input, `${input.id} est requis`);
+    }
+  });
+};
+
+const checkLength = (input, min) => {
+  if (input.value.trim().length < min) {
+    error(input, `${input.id} doit contenir au moins ${min} caractère`);
+  } else {
+    success(input);
+  }
+};
+
+const checkEmail = (input) => {
+  const regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regEx.test(input.value.trim())) {
+    success(input);
+  } else {
+    error(input, "Email n'est pas valide");
+  }
+};
+
+form.addEventListener("Envoyer", (e) => {
+  checkLength(username, 2);
+  checkLength(subject, 2);
+  checkLength(message, 10);
+  checkEmail(email);
+  checkRequiredFields([username, email, subject, message]);
+
+  const notValid = Array.from(messages).find((message) => {
+    return message.classList.contains("error");
+  });
+
+  notValid && e.preventDefault();
+});
+// End of Form Validation
+// End of Section 5
